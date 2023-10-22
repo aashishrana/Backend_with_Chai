00d3cb3d3a7ae546ad47e7428fdbd9ffccc0da8b
aashishrana <aashishrana71135@gmail.com>
2023-10-21 17:27:24 -0700

    A basic backend of MERN project is setup

---
+++ 02-fullStack/backend/server.js
@@ -0,0 +1,35 @@
+import 'dotenv/config'
+// import "dotenv/config"
+import  express  from "express";
+
+const app = express();
+
+app.get("/",(req, res) => {
+    res.send("sita ram");
+});
+
+app.get("/joke", (req, res) => {
+    const jokes = [
+        {
+            id: 1,
+            title: "A joke",
+            content: "This is a joke"
+        },
+        {
+            id: 2,
+            title: "Another joke",
+            content : "This is another joke"
+        },
+        {
+            id: 3,
+            title: "Another title",
+            content: "This is another title"
+        }
+    ]
+    res.send(jokes);
+})
+
+const port = process.env.PORT||5000;
+app.listen(port, ()=> {
+    console.log(`App is running on ${port}`);
+})
