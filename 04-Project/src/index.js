f1d418b2525e95e02fbe160cdfc870b47ac020f1
Aashish rana <aashishrana71135@gmail.com>
2023-11-06 07:18:26 +0000

    seprate database connection file

---
+++ 04-Project/src/index.js
@@ -0,0 +1,57 @@
+import mongoose from "mongoose"
+import { DB_NAME } from "./constants";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+import express from "express"
+const app = express();
+
+
+// function connectDB() {
+
+// }
+// connectDB();
+
+(
+    async() => {
+        try {
+           await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+           app.on("error", (error) => {
+            console.log("Error on server", error);
+            // throw error;
+           })
+
+           app.listen(process.env.PORT, () => {
+            console.log(`App is listening on port : ${process.env.PORT}`);
+
+           })
+
+        } catch (error) {
+            console.error("ERROR: ", error);
+            // throw error
+        }
+
+    }
+)()*/
