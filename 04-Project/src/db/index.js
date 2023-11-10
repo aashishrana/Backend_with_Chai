c635422ede25fbae6a9d3a86c8249613c294746a
Aashish rana <aashishrana71135@gmail.com>
2023-11-10 14:26:05 +0000

    connection errors

--- 04-Project/src/db/index.js
+++ 04-Project/src/db/index.js
@@ -8,7 +8,7 @@ import { DB_NAME } from "../constants.js";
 const connectDb = async () => {
     try {
        const connectionInstance =  await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-       console.log(` MongoDB connected !! DB HOST : ${connectDb}`)
+       console.log(`\n MongoDB connected !! DB HOST : ${connectDb}`);
 
     } catch (error) {
         console.log("MONGODB connection errors", error);
@@ -19,3 +19,6 @@ const connectDb = async () => {
 export default connectDb
 
 // Making a seprate file for the database connection
+
+
+// facing some issue in connection
