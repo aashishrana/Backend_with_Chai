424400680c4817c139ed3593786005dded0a8307
Aashish rana <aashishrana71135@gmail.com>
2023-11-10 14:24:53 +0000

    connection error

--- 04-Project/src/index.js
+++ 04-Project/src/index.js
@@ -3,6 +3,7 @@
 // import mongoose from "mongoose"
 // import { DB_NAME } from "./constants";
 import connectDB from "./db/index.js"
+import dotenv from "dotenv"
 
 dotenv.config({
     path: "./env"
